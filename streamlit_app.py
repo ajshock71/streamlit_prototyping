7b282069cfa3916e3323c69dfc433a29eca246da
ashock <aaron.shock@investorflow.com>
2023-10-21 08:29:17 -0700

    test: more foo

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.warning("... goodbye world")
 st.table(dir(graphviz))
 
 dg = graphviz.Digraph()
-st.write(help_dg.edge))
+st.write(help(dg.edge))
 
 #for (u,d) in [(1,2), (1,3), (2,3), (1,4), (2,4)]:
 #    dg.edge((u, d))
