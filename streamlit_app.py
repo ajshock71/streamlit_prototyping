c2d8297933fea470c4d510e2deaf9ae13302170c
ashock <aaron.shock@investorflow.com>
2023-10-21 08:36:06 -0700

    test: correcting args passed to digraph.edge()

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ st.table(dir(graphviz.Digraph))
 dg = graphviz.Digraph()
 
 for (u,d) in [(1,2), (1,3), (2,3), (1,4), (2,4)]:
-    dg.edge(u, d)
+    dg.edge(str(u), str(d))
 
 st.graphviz_chart(dag)
