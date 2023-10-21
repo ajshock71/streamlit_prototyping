2c3faa8927e0b8b256721ebd9e88267c61103de4
ashock <aaron.shock@investorflow.com>
2023-10-21 08:26:21 -0700

    test: examining graphviz capability in streamlit community cloud

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,10 @@ import graphviz
 import matplotlib
 
 st.warning("... goodbye world")
+st.table(dir(graphviz))
+
+dg = graphviz.Digraph()
+for (u,d) in [(1,2), (1,3), (2,3), (1,4), (2,4)]:
+    dg.add_edge((u, d))
+
+st.graphviz_chart(dag)
